fc21f95c6d0ec582666772c81c9bf641894c03d4
Tailey Eatherton <taileyashlin@gmail.com>
2024-12-02 12:12:33 -0600

    first commit

---
+++ swaybg/.config/swaybg/launch_wallpaper.sh
@@ -0,0 +1,5 @@
+#swaybg --image ~/dotfiles/misc_files/wallpapers/nord_mountains.png
+
+#swaybg --image ~/dotfiles/misc_files/wallpapers/ign_endeavour2.png
+
+swaybg --image ~/Downloads/wallpaper_background/wp6139725-black-and-white-aesthetic-laptop-wallpapers.jpg
